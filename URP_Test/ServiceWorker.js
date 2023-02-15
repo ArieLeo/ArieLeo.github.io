5d4e7e1d508aaccbb6b76cbc0954759ba95d07e3
arie leo <reanimateleo@gmail.com>
2023-02-15 21:02:18 +0800

    Push WebGL URP Test

---
+++ URP_Test/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "DefaultCompany-URP_Stable_Sandbox-0.1";
+const contentToCache = [
+    "Build/URP_Test.loader.js",
+    "Build/URP_Test.framework.js.gz",
+    "Build/URP_Test.data.gz",
+    "Build/URP_Test.wasm.gz",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
